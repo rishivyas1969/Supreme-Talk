9dacc1ef8d6b2303ebc1f71cc7a1eebb1a78f06c
rishi1969 <rishivyas2424@gmail.com>
2021-10-27 19:52:42 +0530

    initial commit

---
+++ code/speechToText.py
@@ -0,0 +1,57 @@
+import speech_recognition as sr
+from datetime import datetime
+
+r = sr.Recognizer()
+texts = []
+adjusted = 0
+
+def searchForPhrase(text):
+
+    if "save me" in text:
+
+        texts[-1] = texts[-1].replace("save me", "")
+
+        with open("supremeText.txt", 'a') as f:
+            now = datetime.now()
+            f.write('\n -- ' + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + " --")
+            for i in  texts:
+                f.write("\n"+i)
+            f.write("\n")
+            print("-- File Saved! --\n")
+        
+        texts.clear()
+
+def adjustNoise(audioFile):
+    with sr.AudioFile(audioFile) as source:
+        r.adjust_for_ambient_noise(source=source)
+    print("Adjusted for noise.")
+
+def convert(audioFile):
+
+    try:
+        with sr.AudioFile(audioFile) as source:
+            if adjusted == 0:
+                adjusted(audioFile)
+
+            audio = r.listen(source)
+
+            text = r.recognize_google(audio)
+            
+            text = text.lower()
+            texts.append(text)
+            if len(texts) > 10:
+                texts.pop(0)
+
+            print(text)
+
+            searchForPhrase(text)
+        return text
+
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+        
+    except sr.UnknownValueError:
+        print("unknown error occured")
+        text = "[ SOME SOUNDS ]"
+        texts.append(text)
+        return text
